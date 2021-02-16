367405fdfa22a623419a29d5cfff1f4e32677856
ammarkandel <merom8703@gmail.com>
2021-02-16 19:00:24 +0200

    add layout and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_201814) do
-
-  create_table "tweets", force: :cascade do |t|
-    t.text "tweet"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
