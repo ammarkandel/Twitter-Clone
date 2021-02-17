5aedb36ab567b3d20286100f8c8b457b4bcedc4f
ammarkandel <merom8703@gmail.com>
2021-02-17 22:47:55 +0200

    fix linter warnings

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -8,5 +8,4 @@ class RegistrationController < Devise::RegistrationController
   def account_update_params
     params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
   end
-
 end
